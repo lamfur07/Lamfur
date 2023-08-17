67303c171bfa0bacf8740ad31009e7c0cb10ba2b
lamfur07 <71093663+lamfur07@users.noreply.github.com>
2023-08-17 17:52:31 +0000

    Changed some stuff

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 import random
 
+
 def get_choices():
   player_choice = input("Enter a choice (rock, paper, scissors): ")
   options = ["rock", "paper", "scissors"]
   computer_choice = random.choice(options)
   choices = {"player": player_choice, "computer": computer_choice}
-
   return choices
+
+
+def check_name(player):
+  if player not in ["rock", "paper", "scissors"]:
+    return
+
 
 def check_win(player, computer):
   print(f"You chose {player} and the computer chose {computer}")
@@ -25,7 +31,10 @@ def check_win(player, computer):
   elif (player == "paper" and computer == "scissors"):
     return f"{computer} wins!!"
   else:
-    return "Enter correct name"
+    return "Enter Correct Choice"
+
 
 choice = get_choices()
+response = check_name(choice["player"])
+print(response)
 print(check_win(choice["player"], choice["computer"]))
